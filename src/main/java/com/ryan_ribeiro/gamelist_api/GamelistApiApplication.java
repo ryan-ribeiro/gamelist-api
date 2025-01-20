cd86b3174aaddaa0d9271cd8b367e914194e11f0
ryan-ribeiro <136587838+ryan-ribeiro@users.noreply.github.com>
2025-01-20 12:43:35 -0300

    Game seed

--- src/main/java/com/ryan_ribeiro/gamelist_api/GamelistApiApplication.java
+++ src/main/java/com/ryan_ribeiro/gamelist_api/GamelistApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ryan_ribeiro.gamelist_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
